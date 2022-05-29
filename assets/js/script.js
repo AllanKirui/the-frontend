77330eadeeb8166c12169d5eef1fbf810d4ab940
AllanKirui <nallawilljr@gmail.com>
2022-05-29 16:58:58 +0300

    Add logic to animate map images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
+// step 1: add logic to show the date
+// - - - - - - - - - - - - - - - - - - -
 const days = [
+  "Sunday",
   "Monday",
   "Tuesday",
   "Wednesday",
   "Thursday",
   "Friday",
   "Saturday",
-  "Sunday",
 ];
 const months = [
   "January",
@@ -27,7 +29,7 @@ const dateEl = document.querySelector(".header__meta-date");
 const setDate = () => {
   let today = new Date();
   const dd = today.getDate();
-  const day = today.getDay() - 1; // Minus 1 because Sunday is 0!
+  const day = today.getDay();
   const mm = today.getMonth();
   const yyyy = today.getFullYear();
 
@@ -36,3 +38,32 @@ const setDate = () => {
 };
 
 setDate();
+
+// step 2: add logic to animate the map image
+// - - - - - - - - - - - - - - - - - - -
+const imageWrapper = document.querySelector(".wrapper__image");
+const mapOutline = document.querySelector(".map-outline");
+let isMouseOver = null;
+let hasMouseLeft = true;
+
+imageWrapper.addEventListener("mouseenter", () => {
+  isMouseOver = true;
+  hasMouseLeft = false;
+  setTimeout(() => {
+    if (isMouseOver && !hasMouseLeft) {
+      mapOutline.classList.remove("animate-leave");
+      mapOutline.classList.add("animate-enter");
+      hasMouseLeft = true;
+    }
+  }, 200);
+});
+
+imageWrapper.addEventListener("mouseleave", () => {
+  isMouseOver = false;
+  setTimeout(() => {
+    if (!isMouseOver && hasMouseLeft) {
+      mapOutline.classList.remove("animate-enter");
+      mapOutline.classList.add("animate-leave");
+    }
+  }, 200);
+});
